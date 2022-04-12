73e016ed7ca08916d2c2d23b300e69d1e76e33c2
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2022-04-12 12:39:38 -0700

    Add files via upload

---
+++ phpindex.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="php.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <form
+      method="get"
+      action="https://my-css-showcase.herokuapp.com/index.html"
+    >
+      <button>Click Me</button>
+    </form>
+  </body>
+</html>
